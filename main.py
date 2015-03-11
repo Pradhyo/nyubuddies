9596f44d14b266b00ac12a3a6cf691020df1cb3d
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-11 14:39:24 -0400

    Add functions to render from template files

--- main.py
+++ main.py
@@ -21,10 +21,21 @@ import jinja2
 template_dir = os.path.join(os.path.dirname(__file__),'templates')
 jinja_env = jinja2.Environment (loader = jinja2.FileSystemLoader(template_dir))
 
-class MainHandler(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self,template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
     def get(self):
-        self.response.write('Find a fellow NYU buddy here :)')
+        self.render("Welcome_Page.html")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)

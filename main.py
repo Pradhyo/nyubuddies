4fe0e63e618b523201accbfdcd0ebe4321a2d878
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-13 18:13:49 -0400

    Put handlers in separate files

--- main.py
+++ main.py
@@ -15,75 +15,8 @@
 # limitations under the License.
 #
 import webapp2
-import os
-import jinja2
-import re
-
-template_dir = os.path.join(os.path.dirname(__file__),'templates')
-jinja_env = jinja2.Environment (loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
-
-class Handler(webapp2.RequestHandler):
-	def write(self, *a, **kw):
-		self.response.out.write(*a, **kw)
-
-	def render_str(self,template, **params):
-		t = jinja_env.get_template(template)
-		return t.render(params)
-
-	def render(self, template, **kw):
-		self.write(self.render_str(template, **kw))
-
-class HomePage(Handler):
-	def get(self):
-		self.render("Home_Page.html")
-
-	def post(self):
-		error = False
-		username = self.request.get('username')
-		password = self.request.get('password')
-		verify = self.request.get('verify')
-		email = self.request.get('email')
-
-		params = dict(username = username,
-					  email = email)
-
-		if not valid_username(username):
-			params['username_error'] = "That's not a valid username"
-			error = True
-
-		if not valid_password(password):
-			params['password_error'] = "That's not a valid password"
-			error = True
-		elif verify != password:
-			params['verify_error'] = "Password mismatch"
-			error = True
-
-		if not valid_email(email):
-			params['email_error'] = "Not a valid email"
-			error = True
-
-		if error:
-			self.render("Home_Page.html", **params)
-		else:
-			self.redirect('/welcome?name=' + username)
-
-
-USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-def valid_username(username):
-	return username and USER_RE.match(username)
-
-PASS_RE = re.compile(r"^.{3,20}$")	
-def valid_password(password):
-	return password and PASS_RE.match(password)
-
-EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')	
-def valid_email(email):
-	return not email or EMAIL_RE.match(email)
-
-class WelcomePage(Handler):
-	def get(self):
-		name = self.request.get("name")
-		self.render("Welcome_Page.html", name = name)
+from homepage import HomePage
+from welcomepage import WelcomePage
 
 app = webapp2.WSGIApplication([('/', HomePage),
 								('/welcome', WelcomePage)], 

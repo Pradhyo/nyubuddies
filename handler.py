5ff667e0412986aec55292ea6b2aea0625ddb890
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-16 15:35:28 -0400

    Create function to make a password hash

--- handler.py
+++ handler.py
@@ -48,3 +48,9 @@ class Handler(webapp2.RequestHandler):
 
 def make_salt(length = 5):
 	return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt = None):
+	if not salt:
+		salt = make_salt()
+	h = hashlib.sha256(name + pw + salt).hexdigest()
+	return '%s,%s' % (salt, h)

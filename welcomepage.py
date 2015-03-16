4db35ff45afef4c155bc380f13cfd2e2242d5f9e
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-16 16:59:47 -0400

    Ensure name passed to /welcome is that of current user based on cookie

--- welcomepage.py
+++ welcomepage.py
@@ -3,4 +3,7 @@ from handler import Handler
 class WelcomePage(Handler):
 	def get(self):
 		name = self.request.get("name")
-		self.render("Welcome_Page.html", name = name)
+		if self.user and self.user.name == name:
+			self.render("Welcome_Page.html", name = self.user.name)
+		else:
+			self.redirect('/signup')

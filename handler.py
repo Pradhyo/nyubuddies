af1b372c22182ac6bd6dc6ab8f08b5d8fed15cda
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-16 15:43:57 -0400

    Add register function to return new user

--- handler.py
+++ handler.py
@@ -77,3 +77,11 @@ class User(db.Model):
 	def by_name(cls, name):
 		u = User.all().filter('name =', name).get()
 		return u
+
+	@classmethod
+	def register(cls, name, pw, email = None):
+		pw_hash = make_pw_hash(name, pw)
+		return User(parent = users_key(),
+					name = name,
+					pw_hash = pw_hash,
+					email = email)

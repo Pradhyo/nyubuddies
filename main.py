6a9f0ea243610b695c21c76b8562976129fa9b18
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-18 00:46:49 -0400

    Add Handler for adding new post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import webapp2
 from homepage import HomePage
-from welcomepage import WelcomePage
+from welcomepage import WelcomePage, NewPost
 from signup import SignUp, EmailConfirmation, ChangePassword
 from handler import LogOut, DeleteAccount
 
@@ -10,5 +10,6 @@ app = webapp2.WSGIApplication([('/', HomePage),
 							   ('/delete_account', DeleteAccount),
 							   ('/change_password', ChangePassword),
 							   ('/email_confirmation', EmailConfirmation),
+							   ('/new_post',NewPost),
 							   ('/welcome', WelcomePage)], 
 							   debug=True)

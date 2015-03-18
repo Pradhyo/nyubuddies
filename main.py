8c80d2e0c8f5ce24f2ce5670e624addc21a3667e
Pradhyo Bijja <pradhyo@nyu.edu>
2015-03-18 12:00:06 -0400

    No handler/links to delete account

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from handler import LogOut, DeleteAccount
 app = webapp2.WSGIApplication([('/', HomePage),
 							   ('/signup', SignUp),
 							   ('/logout', LogOut),
-							   ('/delete_account', DeleteAccount),
+							   #('/delete_account', DeleteAccount),
 							   ('/change_password', ChangePassword),
 							   ('/email_confirmation', EmailConfirmation),
 							   ('/new_post',NewPost),
